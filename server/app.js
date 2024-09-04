a8d6b6bc7dee4f50ba61bc542637302b1ae47283
HariHaran-WebPage <166371805+HariHaran-WebPage@users.noreply.github.com>
2024-09-04 06:30:33 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://quiet-lily-6e9317.netlify.app",
+    origin: "https://wildlenstour.netlify.app",
     // origin: 'http://localhost:5173', // Allow only this origin
     credentials: true, // Allow cookies and authentication headers
   })
